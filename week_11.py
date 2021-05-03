9eeb931e2f534d2567ed1d14d6a5de645561e7c1
rasmus h <cph-rh178@cphbusiness.dk>
2021-05-03 10:36:16 +0200

    a

--- week_11.py
+++ week_11.py
@@ -173,3 +173,18 @@ for classname, correct_count in correct_pred.items():
     accuracy = 100 * float(correct_count) / total_pred[classname]
     print("Accuracy for class {:5s} is: {:.1f} %".format(classname,
                                                    accuracy))
+
+
+
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# Assuming that we are on a CUDA machine, this should print a CUDA device:
+
+print(device)
+
+
+
+net.to(device)
+
+inputs, labels = data[0].to(device), data[1].to(device)
